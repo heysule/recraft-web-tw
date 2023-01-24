4223597b22642f4b394db2d02b6eb0bcaed7c468
Suleiman Yunus <suleiman.yunus@meltwater.org>
2023-01-24 00:34:25 +0000

    feat: add home template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { CategoryFiltersComponent } from './features/products/category-filters/category-filters.component';
 import { ProductOverviewComponent } from './features/products/product-overview/product-overview.component';
+import { HomeComponent } from './features/home/home.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'products', component: CategoryFiltersComponent },
   { path: 'products/:id', component: ProductOverviewComponent },
 ];
