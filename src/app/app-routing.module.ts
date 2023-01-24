15bf476c43ba344274ab21ac123f5f4a34204a06
Suleiman Yunus <suleiman.yunus@meltwater.org>
2023-01-24 01:37:59 +0000

    chore: restructure routing to move to product-listing component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CategoryFiltersComponent } from './features/products/category-filters/category-filters.component';
 import { ProductOverviewComponent } from './features/products/product-overview/product-overview.component';
 import { HomeComponent } from './features/home/home.component';
+import { ProductListComponent } from './features/products/product-list/product-list.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'products', component: CategoryFiltersComponent },
+  { path: 'products', component: ProductListComponent },
   { path: 'products/:id', component: ProductOverviewComponent },
 ];
 
