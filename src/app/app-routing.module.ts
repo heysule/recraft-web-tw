490e9e64f269fcafca1cbb8282764adfe954601c
Suleiman Yunus <suleiman.yunus@meltwater.org>
2023-01-24 02:41:17 +0000

    chore: move cart component into products folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductOverviewComponent } from './features/products/product-overview/product-overview.component';
 import { HomeComponent } from './features/home/home.component';
 import { ProductListComponent } from './features/products/product-list/product-list.component';
+import { CartComponent } from './features/products/cart/cart.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'products', component: ProductListComponent },
   { path: 'products/:id', component: ProductOverviewComponent },
+  { path: 'cart', component: CartComponent },
 ];
 
 @NgModule({
